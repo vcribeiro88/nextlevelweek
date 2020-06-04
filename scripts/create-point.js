c2cdd463c4efa1072325a9a162f82d5422d2fc26
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2020-06-03 20:23:24 -0400

    JavaScript da escolha de estado e cidade na página de cadastro do ponto de coleta

---
+++ scripts/create-point.js
@@ -0,0 +1,33 @@
+function populateStates(){
+    const stateSelect = document.querySelector("select[name=state]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then(res => res.json())
+        .then(states => {
+            for(const state of states){
+                stateSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
+        })
+}
+
+populateStates()
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]")
+    const ufValue = event.target.value
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(cities => {
+            for(const city of cities){
+                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            }
+
+            citySelect.disabled = false
+        })
+}
+
+document
+    .querySelector("select[name = state]")
+    .addEventListener("change", getCities)
