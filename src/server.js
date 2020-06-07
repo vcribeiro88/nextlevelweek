0a06f467be20dfac7716238ccc5533a39cf365a6
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2020-06-07 17:17:51 -0400

    Configuração de rotas

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 const express = require("express")
 const server = express()
 
+// Configurar a pasta public
+server.use(express.static("public"))
+
 // Configuras as rotas
 
 // Página Home
-// Req => Requisição
-// Res => Respostas
 server.get("/", (req, res) =>{
     res.sendFile(__dirname + "/views/index.html")
+})
+
+// Página create point
+server.get("/create-point", (req, res) =>{
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+// Página search results
+server.get("/search-results", (req, res) =>{
+    res.sendFile(__dirname + "/views/search-results.html")
 })
 
 // Ligar servidor
