76551271794c0989a26d48b5fa0d1e204e902514
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2020-06-07 23:28:49 -0400

    Template engine - nunjucks

--- src/server.js
+++ src/server.js
@@ -1,24 +1,31 @@
 const express = require("express")
 const server = express()
+const nunjucks = require("nunjucks")
 
 // Configurar a pasta public
 server.use(express.static("public"))
 
+// Usando template engine NunJucks
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 // Configuras as rotas
 
 // Página Home
-server.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/views/index.html")
+server.get("/", (req, res) => {
+    return res.render("index.html")
 })
 
 // Página create point
-server.get("/create-point", (req, res) =>{
-    res.sendFile(__dirname + "/views/create-point.html")
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
 })
 
 // Página search results
-server.get("/search-results", (req, res) =>{
-    res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 // Ligar servidor
